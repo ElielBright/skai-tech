3e2bb68b67e8b51f989b80a843c781186ac38e64
ElielKwesi <134221255+ElielBright@users.noreply.github.com>
2024-11-08 17:57:37 +0000

    Update VideoSection.jsx

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -5,6 +5,7 @@ function VideoSection() {
   return (
     <div className="video-container">
         <img src="src/assets/WebDev.png" type="video/mp4" />
+      
       <div className="overlay-message">
         <p>You can't become a guru overnight. This is a journey of constant practice and a steep learning curve...</p>
       </div>

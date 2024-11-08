832844ec85537904fe32d25d73c2d22c8db3bd84
ElielKwesi <134221255+ElielBright@users.noreply.github.com>
2024-11-08 18:08:46 +0000

    Update VideoSection.jsx

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function VideoSection() {
   return (
     <div className="video-container">
-        <img src="src/assets/WebDev.png" type="video/mp4" />
+        <img src="src/assets/WebDev.png" type="image" />
       
       <div className="overlay-message">
         <p>You can't become a guru overnight. This is a journey of constant practice and a steep learning curve...</p>

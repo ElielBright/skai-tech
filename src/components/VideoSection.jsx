af12c1f2652cf18f9aa016ce09d9350d36781c89
ElielKwesi <134221255+ElielBright@users.noreply.github.com>
2024-11-08 17:54:36 +0000

    Update VideoSection.jsx

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -4,9 +4,7 @@ import React from 'react';
 function VideoSection() {
   return (
     <div className="video-container">
-      <video id="background-video" loop autoPlay muted>
-        <source src="src/assets/Developer Coding Background.mp4" type="video/mp4" />
-      </video>
+        <img src="src/assets/WebDev.png" type="video/mp4" />
       <div className="overlay-message">
         <p>You can't become a guru overnight. This is a journey of constant practice and a steep learning curve...</p>
       </div>
